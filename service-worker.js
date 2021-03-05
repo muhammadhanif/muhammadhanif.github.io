3065aed40a2f3c4aa768cd0c31bcdd1c89367e80
muhammadhanif <muhammadhanif@users.noreply.github.com>
2021-03-05 23:47:40 +0000

    Update SW

--- service-worker.js
+++ service-worker.js
@@ -1,54 +1,33 @@
-const CACHE_NAME = new Date().toISOString();
-const urlsToCache = [
-  "/assets/css/stylesheet.min.css",
-  "/assets/js/highlight.min.js",
-  "/js/lazysizes.min.js",
-  "/hanifmu-ico.png",
-  "/android-chrome-192x192.png",
-  "/android-chrome-512x512.png",
-  "/images/bg-hanifmu.com.webp",
-  "/manifest.json",
-];
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
+const version = "0.0.0";
+const cacheName = `hanifmu.com-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        "/assets/css/stylesheet.min.css",
+        "/assets/js/highlight.min.js",
+        "/js/lazysizes.min.js",
+        "/hanifmu-ico.png",
+        "/android-chrome-192x192.png",
+        "/android-chrome-512x512.png",
+        "/images/bg-hanifmu.com.webp",
+        "/manifest.json"
+      ])
+          .then(() => self.skipWaiting());
     })
   );
 });
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches
-      .match(event.request, {
-        cacheName: CACHE_NAME,
-      })
-      .then(function (response) {
-        if (response) {
-          console.info(
-            "ServiceWorker: loading assets from cache ",
-            response.url
-          );
-          return response;
-        }
-        console.info(
-          "ServiceWorker: loading assets from server ",
-          event.request.url
-        );
-        return fetch(event.request);
-      })
-  );
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.info("ServiceWorker: cache " + cacheName + " deleted");
-            return caches.delete(cacheName);
-          }
-        })
-      );
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
     })
   );
 });
